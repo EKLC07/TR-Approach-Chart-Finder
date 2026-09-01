4327b7a4c7de0b8eadb3978d600431438aeba0e6
Enes Kılıç <enesk34419@gmail.com>
2026-09-01 16:50:35 +0300

    Delete work directory

--- work/extract_pdf_text.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.stdout.reconfigure(encoding='utf-8', errors='replace')
-from pypdf import PdfReader
-reader = PdfReader(sys.argv[1])
-parts = []
-for page in reader.pages[:2]:
-    parts.append(page.extract_text() or '')
-print('\n'.join(parts)[:12000])
